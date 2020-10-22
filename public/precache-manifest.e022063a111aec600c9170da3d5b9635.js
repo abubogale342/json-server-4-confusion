54fe45f3f0f7bd445ed0a1ca9c069a7b49cd7d66
Habtamu Bogale <abubogale342@gmail.com>
2020-10-22 15:32:02 +0300

    json-server

---
+++ public/precache-manifest.e022063a111aec600c9170da3d5b9635.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc19eae7d6541c42fea4e2c63ddb14b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c0c4f41fb9fcf68b34e",
+    "url": "/static/css/2.58f81ebc.chunk.css"
+  },
+  {
+    "revision": "7a50078967437f916cde",
+    "url": "/static/css/main.26dcd33c.chunk.css"
+  },
+  {
+    "revision": "3c0c4f41fb9fcf68b34e",
+    "url": "/static/js/2.afac0f62.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.afac0f62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a50078967437f916cde",
+    "url": "/static/js/main.a5f6a7fe.chunk.js"
+  },
+  {
+    "revision": "aff20233fc33123ab008",
+    "url": "/static/js/runtime-main.74f98a6f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
